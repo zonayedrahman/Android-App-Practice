643525ffe26e308e67f1d35b15db0c65691d1a1a
zonayedrahman <zr578@nyu.edu>
2022-08-19 11:22:12 -0400

    Added Features
    This improved code now satisfies the first three optional requirements

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +14,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // button reference
-        findViewById<Button>(R.id.button).setOnClickListener{
+        findViewById<Button>(R.id.change_text_color).setOnClickListener{
             findViewById<TextView>(R.id.textView).setTextColor(resources.getColor(R.color.white))
         }
+
+        findViewById<Button>(R.id.change_background).setOnClickListener{
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(resources.getColor(R.color.black))
+            findViewById<TextInputEditText>(R.id.text_edit_field).setBackgroundColor(resources.getColor(R.color.black))
+        }
+
+        findViewById<ConstraintLayout>(R.id.backgroundView).setOnClickListener{
+            findViewById<TextView>(R.id.textView).setTextColor(resources.getColor(R.color.default_color))
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(resources.getColor(R.color.white))
+            findViewById<TextInputEditText>(R.id.text_edit_field).setBackgroundColor(resources.getColor(R.color.white))
+
+        }
+
+        findViewById<Button>(R.id.change_text).setOnClickListener{
+            findViewById<TextView>(R.id.textView).text = "Goodbye 👋"
+        }
+
+
+//        findViewById<TextInputEditText>(R.id.text_edit_field).setOnFocusChangeListener()
 
     }
 }
